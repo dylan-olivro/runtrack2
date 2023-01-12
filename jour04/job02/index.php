422b5a6e44e0689fe4af4c89148eb663f1c44f9c
dylan-olivro <dylan.olivro@laplateforme.io>
2023-01-12 11:06:10 +0100

    ajout du job02

---
+++ jour04/job02/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="" method="get">
+
+        <input type="text" id="firstname" name="firstname" placeholder="firstname">
+
+        <input type="text" id="lastname" name="lastname" placeholder="lastname">
+
+        <input type="submit" name="submit">
+        <br>
+    </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Argument</th>
+                <th>Valeur</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>prenom</td>
+                <td><?php echo $_GET['firstname']; ?></td>
+            </tr>
+            <tr>
+                <td>nom</td>
+                <td><?php echo $_GET['lastname']; ?></td>
+            </tr>
+        </tbody>
+    </table>
+
+
+    <style>
+        table {
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        td,
+        th {
+            border: 1px solid;
+            width: 200px;
+        }
+    </style>
+
+</body>
+
+</html>
