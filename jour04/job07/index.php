9bf4b72239ce116845669868b1f148e34b2e39d4
dylan-olivro <dylan.olivro@laplateforme.io>
2023-01-13 10:34:05 +0100

    ajout du job07 fini

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -1,36 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
 
 <body>
-    <style>
-        body {
-            text-align: center;
-        }
-    </style>
-    <form action="" method="get">
+    <form action="" method="get" class="formulaire">
+        <label for="largeur">Largeur : </label>
+        <input type="text" name="largeur" id="largeur">
 
-        <input type="text" id="hauteur" name="hauteur" placeholder="hauteurs">
+        <label for="hauteur">Hauteur : </label>
+        <input type="text" name="hauteur" id="hauteur">
 
-        <input type="submit">
-        <br>
+        <input type="submit" value="Envoyer">
     </form>
-    <?php
-    for ($i = 0; $i < $_GET['hauteur']; $i++) {
-        for ($k = ($_GET['hauteur'] - $i); $k > 0; $k--) {
-            echo " ";
-        }
-        for ($z = 0; $z <= $i; $z++) {
-            echo "*";
-        }
-        echo "<br />";
-    } ?>
+
 </body>
 
 </html>
+
+<pre>
+<?php
+
+if (isset($_GET['largeur']) && isset($_GET['hauteur'])) {
+    // For the triangle
+    $x = 1;
+    $h = $_GET['hauteur'];
+
+    while ($x <= $_GET['hauteur']) {
+        // To print whitespaces at the begining of each line
+        for ($i = 0; $i <= $_GET['hauteur'] - $x; $i++) {
+            echo "&nbsp;";
+        }
+        echo "/";
+
+        for ($j = 0; $j < $_GET['hauteur'] - $h; $j++) {
+            echo "_";
+        }
+        if ($_GET['hauteur'] - $x == 0) {
+            echo "\\"; // print "\" at the end of last line (no <br/>)
+        } else {
+            echo "\\<br/>";
+        }
+        $x++;
+        $h -= 2;
+    }
+
+
+
+
+    // For the square
+    echo "<br/>";
+
+
+    for ($i = 1; $i < $_GET['hauteur']; $i++) {
+        echo "|";
+        for ($j = 0; $j < $_GET['largeur']; $j++) {
+            echo "&nbsp;";
+        }
+        echo "|<br/>";
+    }
+    echo "|";
+    for ($i = 0; $i < $_GET['largeur']; $i++) {
+        echo "_";
+    }
+    echo "|<br/>";
+}
+?>
+</pre>
